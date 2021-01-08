f6447efcdb63fd6e775bcef481bbad9b0c1e19d6
sherlock wisdom <wisdomnji@gmail.com>
2021-01-08 19:54:52 +0100

    modem parser added

--- mmcli-parser.py
+++ mmcli-parser.py
@@ -25,6 +25,26 @@ class Modems():
 
         return modems
 
+class Modem():
+    def __init__( self, index ):
+        self.mmcli_m = ["mmcli", f"-Km", index]
+
+    def m(self):
+        mmcli_output = subprocess.check_output(self.mmcli_m).decode('utf-8')
+        mmcli_output = mmcli_output.split('\n')
+
+        m_details = {}
+        for output in mmcli_output:
+            m_detail = output.split(': ')
+            if len(m_detail) < 2:
+                continue
+            m_details[m_detail[0].replace(' ', '')] = m_detail[1]
+
+        return m_details
+
 
 modems = Modems()
 print(f">>Modems indexes: {modems.L()}")
+for index in modems.L():
+    modem = Modem( index )
+    print(f">> Modem[{index}]\n{modem.m()}")
