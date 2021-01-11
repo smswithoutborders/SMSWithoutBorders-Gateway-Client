ea52e2a6955460ec7d106df961a1623ebccb29d8
Sherlock Wisdom <wisdomnji>
2021-01-11 23:58:13 +0100

    updated ussd commands

--- ussd.py
+++ ussd.py
@@ -46,9 +46,31 @@ class USSD():
         
         return False
 
+
+    def get(self, key):
+        return {
+                "status" : "modem.3gpp.ussd.status", 
+        }[key]
+
     def status(self):
         ussd_command = self.modem.mmcli_m + [f"--3gpp-ussd-status"]
 
+        try: 
+            mmcli_output = subprocess.check_output(ussd_command, stderr=subprocess.STDOUT).decode('utf-8')
+        except subprocess.CalledProcessError as error:
+            print(f"[stderr]>> return code[{error.returncode}], output[{error.output.decode('utf-8')}")
+        else:
+            # print(f"mmcli_output: {mmcli_output}")
+            mmcli_output = mmcli_output.split('\n')
+            s_details = {}
+            for output in mmcli_output:
+                s_detail = output.split(': ')
+                if len(s_detail) < 2:
+                    continue
+                key = s_detail[0].replace(' ', '')
+                s_details[key] = s_detail[1]
+
+            return s_details
 
 if __name__ == "__main__":
     modem = Modem("0")
@@ -62,6 +84,8 @@ if __name__ == "__main__":
 
         respond = ussd.respond( "4" )
         print(respond)
+
+        print(ussd.status()[ussd.get("status")])
 
         ussd.cancel()
     except Exception as error:
