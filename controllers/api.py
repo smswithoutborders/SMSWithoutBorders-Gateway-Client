23656c06e63d1ed3164bb36c15f5a5a5a9601fb7
sherlock wisdom <wisdomnji@gmail.com>
2021-01-29 15:44:32 +0100

    ready python to do some good work

--- controllers/api.py
+++ controllers/api.py
@@ -3,11 +3,20 @@
 
 import configparser
 CONFIGS = configparser.ConfigParser(interpolation=None)
-CONFIGS.read("config.ini")
+
+if __name__ == "__main__":
+    CONFIGS.read("config.ini")
+    from ..modules.messagestore import MessageStore
+else:
+    CONFIGS.read("controllers/config.ini")
+    from modules.messagestore import MessageStore
 
 
 from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+messageStore = MessageStore()
+# messageStore = MessageStore(config=CONFIGS)
 
 # Get current state of the daemon [idle, busy, help]
 @app.route('/state')
@@ -17,7 +26,20 @@ def daemon_state():
 @app.route('/messages', methods=['POST'])
 def new_messages():
     request_body = request.json
-    return jsonify({"status": 200})
+    if not 'text' in request_body:
+        return jsonify({"status":400, "message":"missing text"})
+
+    if not 'number' in request_body:
+        return jsonify({"status":400, "message":"missing number"})
+
+    text = request_body["text"]
+    number = request_body["number"]
+    
+    # TODO: put logger in here to log everything
+    print(f"[+] New message...\n\t-text: {text}\n\t-number: {number}")
+
+    tstate = MessageStore.insert({"text":text, "number":number})
+    return jsonify({"status": 200, "tstate":tstate})
 
 if CONFIGS["API"]["DEBUG"] == "1":
     # Allows server reload once code changes
