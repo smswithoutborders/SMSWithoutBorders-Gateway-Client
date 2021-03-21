d0461d583b4a41fd05ba68d84716f0d0cdb2c87b
Sherlock <wisdomnji@gmail.com>
2021-03-21 18:59:07 +0100

    updated router

--- src/api.py
+++ src/api.py
@@ -39,29 +39,36 @@ def new_bulk_message():
 
     return jsonify({"status":200, "c_tstate":c_tsate})
 
-@app.route('/messages', methods=['POST'])
+@app.route('/messages', methods=['POST', 'GET'])
 def new_messages():
-    request_body = request.json
-    if not 'text' in request_body:
-        return jsonify({"status":400, "message":"missing text"})
-
-    if not 'phonenumber' in request_body:
-        return jsonify({"status":400, "message":"missing phonenumber"})
-
-    text = request_body["text"]
-    phonenumber = request_body["phonenumber"]
     
-    # TODO: put logger in here to log everything
-    print(f"[+] New message...\n\t-text: {text}\n\t-phonenumber: {phonenumber}")
 
-    return_json = {"status" :"", "tstate":""}
-    try: 
-        messageID = datastore.new_message(text=text, phonenumber=phonenumber, isp="MTN")
-        return_json["status"] = 200
-        return_json["messageID"] = messageID
-    except Exception as err:
-        print( f"[err]: {err}" )
+    if request.method == 'POST':
+        request_body = request.json
+        if not 'text' in request_body:
+            return jsonify({"status":400, "message":"missing text"})
+
+        if not 'phonenumber' in request_body:
+            return jsonify({"status":400, "message":"missing phonenumber"})
+
+        text = request_body["text"]
+        phonenumber = request_body["phonenumber"]
+        
+        # TODO: put logger in here to log everything
+        print(f"[+] New message...\n\t-text: {text}\n\t-phonenumber: {phonenumber}")
+
+        return_json = {"status" :"", "tstate":""}
+        try: 
+            messageID = datastore.new_message(text=text, phonenumber=phonenumber, isp="MTN")
+            return_json["status"] = 200
+            return_json["messageID"] = messageID
+        except Exception as err:
+            print( f"[err]: {err}" )
     
+
+    elif request.method == 'GET':
+        print("[?] Fetching messages....")
+
     return jsonify(return_json)
 
 if CONFIGS["API"]["DEBUG"] == "1":
