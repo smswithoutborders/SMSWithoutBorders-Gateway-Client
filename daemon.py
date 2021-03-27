f60e68de8b8c7d52a3eb85dbd7d261b485710f9f
Sherlock Wisdom <wisdomnji@gmail.com>
2021-03-27 11:25:46 +0100

    broken: soft links not working as intended, check configs fail

--- daemon.py
+++ daemon.py
@@ -9,23 +9,24 @@ import logging
 import threading
 
 
-format = "[%(asctime)s] >> %(message)s"
-logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
-try:
-    check_state = start_routines.sr_database_checks()
-    if not check_state:
-        logging.info("\t- Start routine check failed...")
-except Exception as error:
-    # print("Error raised:", error)
-    print( error )
-    exit()
+def start():
+    format = "[%(asctime)s] >> %(message)s"
+    logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
+    try:
+        check_state = start_routines.sr_database_checks()
+        if not check_state:
+            logging.info("\t- Start routine check failed...")
+    except Exception as error:
+        # print("Error raised:", error)
+        print( error )
+        exit()
 
-else:
-    send_thread = threading.Thread(target=sms_send.daemon, daemon=True)
-    read_thread = threading.Thread(target=sms_read.daemon, daemon=True)
+    else:
+        send_thread = threading.Thread(target=sms_send.daemon, daemon=True)
+        read_thread = threading.Thread(target=sms_read.daemon, daemon=True)
 
-    send_thread.start()
-    read_thread.start()
+        send_thread.start()
+        read_thread.start()
 
-    send_thread.join()
-    read_thread.join()
+        send_thread.join()
+        read_thread.join()
