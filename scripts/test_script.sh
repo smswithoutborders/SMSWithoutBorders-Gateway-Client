a34de26fdd29b5e6edb823a69460b284586a4b67
Sherlock Wisdom <wisdomnji@gmail.com>
2021-04-15 21:57:40 +0100

    updated: added test for checking if routes do send messages

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -4,6 +4,9 @@
 url="http://127.0.0.1:6868"
 d_command=$1
 phonenumber=$2
+email=$3
+
+$gmail_templated_message="gmail:send:Afkanerd progressing on SMSwithoutborders:$3:Hi Nerds,\nThis is a test message! If you are reading this, it means it was successful!\nBest Works\n-Wisdom @sw\\0b"
 
 if [ "$d_command" == "--send" ] ; then
 	if [ "$phonenumber" == "" ] ; then
@@ -25,4 +28,8 @@ elif [ "$d_command" == "--state" ] ; then
 elif [ "$d_command" == "--logs" ] ; then
 	echo ">> Fetching logs..."
 	curl -X GET "${url}/logs"
+
+elif [ "$d_command" == "--route" ] ; then
+	echo ">> Routing..."
+	mysql -u root -p -e "INSERT INTO messages SET text='$gmail_templated_message',phonenumber='$2',type='routing'"
 fi
