25ec37b9a2a064fa5a76bd9922f6e64660c461ea
Sherlock Wisdom <wisdomnji@gmail.com>
2021-04-16 12:32:34 +0100

    updated test script

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -3,18 +3,17 @@
 
 url="http://127.0.0.1:6868"
 d_command=$1
-phonenumber=$2
-email=$3
+protocol=$2
 
-$gmail_templated_message="gmail:send:Afkanerd progressing on SMSwithoutborders:$3:Hi Nerds,\nThis is a test message! If you are reading this, it means it was successful!\nBest Works\n-Wisdom @sw\\0b"
+gmail_templated_message="gmail:send:Afkanerd progressing on SMSwithoutborders:$2:Hi Nerds,\nThis is a test message! If you are reading this, it means it was successful!\nBest Works\n-Wisdom @sw\\0b"
 
 if [ "$d_command" == "--send" ] ; then
-	if [ "$phonenumber" == "" ] ; then
+	if [ "$protocol" == "" ] ; then
 		echo "Phone number required but not provided.."
 	else
 		echo ">> Sending..."
 		# date=$(date)
-		curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"$(date)\",\"phonenumber\":\"${phonenumber}\"}" "${url}/messages"
+		curl -X POST -H "Content-Type: application/json" -d "{\"text\":\"$(date)\",\"phonenumber\":\"${protocol}\"}" "${url}/messages"
 	fi
 
 elif [ "$d_command" == "--received" ] ; then
@@ -31,5 +30,5 @@ elif [ "$d_command" == "--logs" ] ; then
 
 elif [ "$d_command" == "--route" ] ; then
 	echo ">> Routing..."
-	mysql -u root -p -e "INSERT INTO messages SET text='$gmail_templated_message',phonenumber='$2',type='routing'"
+	mysql -u root -p -e "INSERT INTO deku.messages SET text='$gmail_templated_message',phonenumber='$2',type='routing'"
 fi
