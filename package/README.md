730b6985906b80aeb677cfcd3830e581a111b414
Sherlock Wisdom <wisdomnji@gmail.com>
2021-08-10 20:59:32 +0100

    update: fixed submodules - shold look for better way of doing this

--- package/README.md
+++
@@ -1,4 +0,0 @@
-<h3>Lightweight edition</h3>
-- Remove bloatware (MySQL)
-- Persist with wanting to send and response in accordance
-- Built to be extenable
--- services/node.py
+++ services/node.py
@@ -13,17 +13,19 @@ sys.path.append(os.path.abspath(os.getcwd()))
 from mmcli_python.modem import Modem
 
 class Node:
-    def __init__(self, m_index, queue_name='', exchange_type='topic'):
+    def __init__(self, m_index, queue_name='', exchange_type='direct'):
         ''' 
         queue_name : should be name of isp - no cus this would delete the entirty when 
         closed
         '''
 
+        # TODO: put in config
         self.exchange='sms'
         self.m_index=m_index
         self.queue_name=queue_name
         self.exchange_type=exchange_type
 
+        # TODO: put in config
         connection=pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel=connection.channel()
 
@@ -39,8 +41,19 @@ class Node:
 
         ''' consumer properties '''
         ''' no auto_ack '''
+
+        # TODO: delete this, ack should be manual
         self.channel.basic_consume(
                 queue=self.queue_name, on_message_callback=self.__callback, auto_ack=True)
+
+        '''
+        self.channel.basic_consume(
+                queue=self.queue_name, on_message_callback=self.__callback)
+        '''
+
+        ''' set fair dispatch '''
+        # TODO: put in config
+        self.channel.basic_qos(prefetch_count=1)
 
     def __callback(self, ch, method, properties, body):
         print(f'\t* message: {body}')
@@ -62,7 +75,7 @@ if __name__ == "__main__":
 
     for m_index in indexes:
         # isp_name=Tools.ISP.modem('cameroon', '')
-        isp_name="MTN"
+        isp_name="MTN1"
         print('\t* starting broker for:', m_index, isp_name)
         node=Node(m_index, isp_name)
         node.start_consuming()
