679835380325a837b0ce1087d0e5aef4fada218f
Sherlock Wisdom <wisdomnji@gmail.com>
2021-08-10 20:33:15 +0100

    update: basic consumption

---
+++ services/node.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+''' testing criterias----
+- when modem is present a broker should start up
+    - should be able to receive messages from a producer
+'''
+
+import sys, os
+
+import pika
+
+sys.path.append(os.path.abspath(os.getcwd()))
+from mmcli_python.modem import Modem
+
+class Node:
+    def __init__(self, m_index, queue_name='', exchange_type='topic'):
+        ''' 
+        queue_name : should be name of isp - no cus this would delete the entirty when 
+        closed
+        '''
+
+        self.exchange='sms'
+        self.m_index=m_index
+        self.queue_name=queue_name
+        self.exchange_type=exchange_type
+
+        connection=pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel=connection.channel()
+
+        ''' listening exchange '''
+        self.channel.exchange_declare(exchange=self.exchange, exchange_type=self.exchange_type)
+
+        ''' listening queue '''
+        # result=channel.queue_declare(queue=queue_name, exclusive=True)
+        self.channel.queue_declare(queue=self.queue_name)
+
+        ''' bind queue to exchange '''
+        self.channel.queue_bind(exchange=self.exchange, queue=self.queue_name)
+
+        ''' consumer properties '''
+        ''' no auto_ack '''
+        self.channel.basic_consume(
+                queue=self.queue_name, on_message_callback=self.__callback, auto_ack=True)
+
+    def __callback(self, ch, method, properties, body):
+        print(f'\t* message: {body}')
+
+    def start_consuming(self):
+        print(f'\t[{self.m_index}]* waiting for message...')
+        self.channel.start_consuming()
+
+if __name__ == "__main__":
+    ''' - when modem open worker
+        - when no worker close worker
+    '''
+    def available_modem():
+        return Modem.list()
+
+    # indexes=available_modem()
+    indexes=['1']
+    print('* starting brokers for modems with indexes:', indexes)
+
+    for m_index in indexes:
+        # isp_name=Tools.ISP.modem('cameroon', '')
+        isp_name="MTN"
+        print('\t* starting broker for:', m_index, isp_name)
+        node=Node(m_index, isp_name)
+        node.start_consuming()
