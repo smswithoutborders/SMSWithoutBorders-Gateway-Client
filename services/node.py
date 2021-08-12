5d501bcaa6c77662b6bd4ef9bd8b24c4e2656346
Sherlock Wisdom <wisdomnji@gmail.com>
2021-08-12 01:21:42 +0100

    update: basic configs working now

--- services/node.py
+++ services/node.py
@@ -5,7 +5,40 @@
     - should be able to receive messages from a producer
 '''
 
-import sys, os, threading
+''' protocol
+- queues hold messages
+- nodes listen on queues
+- messages are routed to queues from exchanges
+
+- gateway states
+    - online -> messages come directly from server (this is the case we're looking at)
+    - offline -> messages come from sms services like Twilio
+
+- user_id:user_key = exchange
+- nodes id becomes topics to listen to
+
+<issues aren't what the nodes want to hear, but what the clients are sending>
+
+## exchange and queue should be static and known only to the host
+exchange_name="DEKU_CLUSTER"
+queue_name="OUTGOING_SMS"
+
+exchange_name = <would depend on the server configuration>
+queue_name = <would depend on the server configuration>
+
+exchange type: topic
+topic methods
+-> 12345.1a2b3c.MTN
+-> dev_id.node_id.isp
+'''
+
+''' TODO <lookup>:
+- authenticate nodes when connecting
+- make sure messages are not persistent
+- stop nodes from declaring exchange and queues
+'''
+
+import sys, os, threading, traceback
 import asyncio, configparser
 
 import pika
@@ -13,42 +46,51 @@ import pika
 sys.path.append(os.path.abspath(os.getcwd()))
 from mmcli_python.modem import Modem
 
+connection=None
+
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), '', 'config.ini'))
+try:
+    connection_url = config['NODE']['connection_url']
+    connection=pika.BlockingConnection(pika.ConnectionParameters(connection_url))
+except Exception as error:
+    print(traceback.format_exc())
+    sys.exit(1)
+
 class Node:
-    def __init__(self, m_index, queue_name='', exchange_type='direct'):
-        ''' bringing config in here means it's hot reloaded when nodes are created '''
-        config = configparser.ConfigParser()
-        config.read(os.path.join(os.path.dirname(__file__), '', 'config.ini'))
+    def __init__(self, m_index, m_isp, exchange_type=config['NODE']['exchange_type']):
 
         ''' 
         queue_name : should be name of isp - no cus this would delete the entirty when 
         closed
         '''
-
-        self.exchange=config['NODE']['exchange']
         self.m_index=m_index
-        self.queue_name=queue_name
-        self.exchange_type=exchange_type
-        connection_url = config['NODE']['connection_url']
 
-        connection=pika.BlockingConnection(pika.ConnectionParameters(connection_url))
+        self.exchange=config['NODE']['exchange_name']
+        self.queue_name=config['NODE']['queue_name']
+        self.binding_key=f"{config['NODE']['binding_key']}.{m_isp}"
         self.channel=connection.channel()
 
-        ''' listening exchange creation on the stack '''
-        self.channel.exchange_declare(exchange=self.exchange, exchange_type=self.exchange_type)
-
-        ''' listening queue creation on the stack'''
-        # result=channel.queue_declare(queue=queue_name, exclusive=True)
-        self.channel.queue_declare(queue=self.queue_name)
-
         ''' bind queue to exchange '''
-        self.channel.queue_bind(exchange=self.exchange, queue=self.queue_name)
+        try:
+
+            self.channel.queue_bind(
+                    exchange=self.exchange, 
+                    queue=self.queue_name, 
+                    routing_key=self.binding_key)
+
+        except pika.exceptions.ChannelClosedByBroker as error:
+            print(error)
+            sys.exit(1)
 
         ''' consumer properties '''
         ''' no auto_ack '''
 
         # TODO: delete this, ack should be manual
         self.channel.basic_consume(
-                queue=self.queue_name, on_message_callback=self.__callback, auto_ack=True)
+                queue=self.queue_name, 
+                on_message_callback=self.__callback, 
+                auto_ack=bool(int(config['NODE']['auto_ack'])))
 
         '''
         self.channel.basic_consume(
@@ -66,23 +108,18 @@ class Node:
         self.channel.start_consuming()
 
 def main():
-    ''' - when modem open worker
-        - when no worker close worker
-    '''
-    def available_modem():
-        return Modem.list()
-
+    l_threads=[]
     # indexes=available_modem()
     indexes=['1', '2']
     print('* starting consumer for modems with indexes:', indexes)
 
-    l_threads=[]
     for m_index in indexes:
         # isp_name=Tools.ISP.modem('cameroon', '')
-        isp_name="MTN1"
-        print('\t* starting consumer for:', m_index, isp_name)
+        m_isp="MTN1"
 
-        node=Node(m_index, isp_name)
+        print('\t* starting consumer for:', m_index, m_isp)
+        node=Node(m_index, m_isp)
+
         thread=threading.Thread(target=node.start_consuming, daemon=True)
         l_threads.append(thread)
 
@@ -93,5 +130,5 @@ def main():
         thread.join()
 
 if __name__ == "__main__":
-   main()
+    main()
 
