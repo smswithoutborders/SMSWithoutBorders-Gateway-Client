6ee6db0c3b7c510f3e026178582596316e58881d
Wisdom Nji <wisdomnji@gmail.com>
2021-10-25 13:12:35 +0100

    update: fixed cli output, added more commands to deku, guranteed labels work

--- services/deku.py
+++ services/deku.py
@@ -79,7 +79,7 @@ class Deku(Modem):
 
         
     @classmethod
-    def modem_locked(cls, identifier, id_type:Modem.IDENTIFIERS=Modem.IDENTIFIERS.IMEI, benchmark_remove=True):
+    def modem_locked(cls, identifier, id_type:Modem.IDENTIFIERS=Modem.IDENTIFIERS.IMEI, remove_lock=True):
         '''
         pdu-type might be what determines the kind of message this is
         - check latest message, if pdu-type == submit
@@ -126,7 +126,7 @@ class Deku(Modem):
             busy_timelimit = int(cls.config['MODEMS']['busy_benchmark_limit'])
 
             # print(f'lt: {lock_type}\nnow: {time.time()}\nstart_time: {start_time}\ndiff: {time.time() - start_time}')
-            if benchmark_remove and ((time.time() - start_time ) > benchmark_timelimit and lock_type == 'BENCHMARK') or ((time.time() - start_time ) > busy_timelimit and lock_type == 'BUSY'): #seconds
+            if remove_lock and ((time.time() - start_time ) > benchmark_timelimit and lock_type == 'BENCHMARK') or ((time.time() - start_time ) > busy_timelimit and lock_type == 'BUSY'): #seconds
                 # print('\ttype = benchmark')
                 ''' set the file free '''
                 os.remove(lock_dir)
@@ -179,7 +179,7 @@ class Deku(Modem):
         return messages
 
     @staticmethod
-    def modems_ready(isp=None, country=None, m_index=None, remove_lock=False):
+    def modems_ready(isp=None, country=None, m_index=None, remove_lock=False, ignore_lock=False):
         available_indexes=[]
         # print('fetching available modems')
 
@@ -199,10 +199,11 @@ class Deku(Modem):
             # print(Deku.ISP.__dict__)
             # print(country)
 
-            locked_state, lock_type, lock_file = Deku.modem_locked(Modem(_m_index).imei)
+            locked_state, lock_type, lock_file = Deku.modem_locked(Modem(_m_index).imei, remove_lock=remove_lock)
 
-            if locked_state:
-                return available_indexes
+            if not ignore_lock and locked_state:
+                # return available_indexes
+                continue
 
             ''' should be in setting before deciding to use isp checking here -
             credit is still a viable option '''
@@ -351,7 +352,7 @@ class Deku(Modem):
 
     @staticmethod
     def modem(modem_index):
-        return Modem(index)
+        return Modem(modem_index)
 
     @classmethod
     def __init__(cls):
@@ -385,9 +386,17 @@ class Deku(Modem):
                 output=[cmd, cls.USSD(Modem(modem_index)).respond(cmd)]
                 # print(output)
                 ussd_output.append(output)
+        except cls.USSD.UnknownError as error:
+            raise(error)
+        except cls.USSD.CannotInitiateUSSD as error:
+            # print("- output:", error.output)
+            # print("- command:", error.command)
+            raise(error)
         except subprocess.CalledProcessError as error:
             # print("::Error>", error.cmd, error.output)
             # print(traceback.format_exc())
+            # print(error.output)
+            # error.output = ussd_output
             raise(error)
 
         return ussd_output
@@ -402,7 +411,11 @@ class Deku(Modem):
             if command[0] == 'ussd':
                 try:
                     return cls.cli_parse_ussd(modem_index, command[1])
+                except cls.USSD.CannotInitiateUSSD as error:
+                    raise(error)
                 except subprocess.CalledProcessError as error:
+                    # print(error.output)
+                    # print(error.output)
                     raise(error)
             else:
                 print("** unknown label command requested")
@@ -412,6 +425,9 @@ class Deku(Modem):
         country=cls.config['ISP']['country']
         modem_isp = cls.ISP.modems(operator_code=Modem(modem_index).operator_code, country=country)
         print("modem isp:", modem_isp)
+        if modem_isp is None:
+            print("* cannot determine modem's isp")
+            return None
 
         ''' check of for labels that match the requested labels, 
         then execute for the matching isp for that label '''
@@ -425,24 +441,27 @@ class Deku(Modem):
             return execute_command(label_config[label][modem_isp])
         else:
             print(f"** no label({label}) in labels file")
-            
+
 
 if __name__ == "__main__":
     ''' should use command line arg parser here '''
     modem_index=None
     ussd_command=None
     label_command=None
-    for i in range(len(sys.argv[1:])):
+    for i in range(1, len(sys.argv)):
         arg = sys.argv[i]
+        # print('arg:', arg)
         if arg == '--modem' or arg == '-m':
             modem_index = sys.argv[i+1]
-            i += 1
+            # i += 1
         elif arg == '--ussd':
             ussd_command = sys.argv[i+1]
-            i += 1
+            # i += 1
+        elif arg == '--ussd-cancel':
+            ussd_command = arg
         elif arg == '--label':
             label_command = sys.argv[i+1]
-            i += 1
+            # i += 1
 
     if modem_index == None:
         print("usage: --[modem_index] --[option] [value]")
@@ -450,13 +469,28 @@ if __name__ == "__main__":
 
 
     if ussd_command is not None:
-        print(f"* Dailing USSD - {ussd_command}")
-        ussd_output=Deku.cli_parse_ussd(modem_index, ussd_command)
-        print(ussd_output)
+        if ussd_command == '--ussd-cancel':
+            print("* cancelling USSD command...")
+            try:
+                Modem(modem_index).USSD.cancel()
+            except Deku.USSD.UnknownError as error:
+                print("USSD error:", str(error.output))
+                exit(1)
+        else:
+            print(f"* Dailing USSD - {ussd_command}")
+            ussd_output=Deku.cli_parse_ussd(modem_index, ussd_command)
+            print(ussd_output)
 
     elif label_command is not None:
         print(f"* Executing label command - {label_command}")
-        label_output=Deku().cli_parse_labels(modem_index, label_command)
-        print(label_output)
+        try:
+            label_output=Deku().cli_parse_labels(modem_index, label_command)
+        except Deku.USSD.CannotInitiateUSSD as error:
+            # print(error.output)
+            print("USSD error:", str(error.output))
+            # raise(error)
+            exit(1)
+        else:
+            print(label_output)
 
-
+    exit(0)
