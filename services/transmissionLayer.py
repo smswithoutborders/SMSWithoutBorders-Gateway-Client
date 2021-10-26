c3d113cd5bbf63f9c3b4c65e9ff621c26176e97f
Wisdom Nji <wisdomnji@gmail.com>
2021-10-26 14:32:00 +0100

    update: transmission protocol works

--- services/transmissionLayer.py
+++ services/transmissionLayer.py
@@ -3,19 +3,20 @@
 ''' Telegram '''
 from transmissionLayer_telegram import TelegramTransmissionLayer
 
-
 class TransmissionLayer:
-    def __init__(self):
+    @classmethod
+    def __init__(cls):
         ''' declare your layers here '''
-        self.telegram = TelegramTransmissionLayer()
+        cls.telegram = TelegramTransmissionLayer()
 
         ''' if more layers are added, they should be appended to this list '''
-        self.transmission_layers = []
-        self.transmission_layers.append(self.telegram)
+        cls.transmission_layers = []
+        cls.transmission_layers.append(cls.telegram)
 
     # def send(self, data, tranmission_platform='telegram'):
-    def send(self, data):
-        for platform in self.transmission_layers:
+    @classmethod
+    def send(cls, data):
+        for platform in cls.transmission_layers:
             ''' funny things are expected to happen here, 
             catch as many as possible '''
             platform.send(data)
@@ -27,9 +28,9 @@ if __name__ == "__main__":
     -> which ever transmission layer has all the required settings gets used
     '''
 
-    transmissionLayer = TransmissionLayer()
+    # transmissionLayer = TransmissionLayer()
 
     # print(tranmissionLayer.list_layers())
     ''' send is inherited from each of the layers, so the logic is based on the layer's code '''
     ''' things could happen, so have the exceptions for when they do ready '''
-    transmissionLayer.send("hello world")
+    TransmissionLayer().send("hello world")
