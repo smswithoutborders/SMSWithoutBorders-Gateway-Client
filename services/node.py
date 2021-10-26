8220aafaf72681ccdbf4aee69490db4bd19dbb0a
Wisdom Nji <wisdomnji@gmail.com>
2021-10-26 22:53:55 +0100

    update: removed all gateway code... technically don't make sense to be on node

--- services/node.py
+++ services/node.py
@@ -28,9 +28,7 @@ from transmissionLayer import TransmissionLayer
     - offline -> messages come from sms services like Twilio
 
 - user_id:user_key = exchange
-- nodes id becomes topics to listen to
-
-<issues aren't what the nodes want to hear, but what the clients are sending>
+- nodes id becomes topics to listen to <issues aren't what the nodes want to hear, but what the clients are sending>
 
 ## exchange and queue should be static and known only to the host
 exchange_name="DEKU_CLUSTER"
@@ -93,8 +91,6 @@ class Node:
 
     outgoing_connection=None
     outgoing_channel=None
-    routing_consume_connection=None
-    routing_consume_channel=None
 
     previousError=None
 
@@ -120,7 +116,7 @@ class Node:
 
     # def __init__(self, m_index, m_isp, rules=['STATUS']):
     # TODO: everything from config files should be externally sent and not read in the class
-    def __init__(self, m_index, m_isp, config, config_event_rules, start_router=True):
+    def __init__(self, m_index, m_isp, config, config_event_rules):
         self.config_event_rules = config_event_rules
         self.config = config
         def create_channel(connection_url, queue_name, username=None, password=None, exchange_name=None, exchange_type=None, durable=False, binding_key=None, callback=None, prefetch_count=0):
@@ -223,29 +219,6 @@ class Node:
             # print(error)
             # if error == "[Errno -2] Name or service not known":
             raise(error)
-
-        if start_router:
-            try:
-                self.routing_consume_connection, self.routing_consume_channel = create_channel(
-                        connection_url=config['NODE']['connection_url'],
-                        callback=self.__sms_routing_callback,
-                        durable=True,
-                        prefetch_count=1,
-                        queue_name=config['NODE']['routing_queue_name'])
-            except pika.exceptions.ConnectionClosedByBroker:
-                raise(error)
-            except pika.exceptions.AMQPChannelError as error:
-                # self.logger("Caught a chanel error: {}, stopping...".format(error))
-                raise(error)
-            except pika.exceptions.AMQPConnectionError as error:
-                # self.logger("Connection was closed, should retry...")
-                raise(error)
-            except socket.gaierror as error:
-                # print(error.__doc__)
-                # print(type(error))
-                # print(error)
-                # if error == "[Errno -2] Name or service not known":
-                raise(error)
         
     def __del__(self):
         # self.logger("calling destructor", output="stderr")
@@ -372,78 +345,6 @@ class Node:
                 ''' in this case, reset the counter '''
             """
 
-
-    def __sms_routing_callback(self, ch, method, properties, body):
-        json_body = json.loads(body.decode('unicode_escape'))
-        self.logger(f'routing: {json_body}')
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-        """
-        ''' attempts both forms of routing, then decides if success or failed '''
-        ''' checks config if for which state of routing is activated '''
-        ''' if online only, if offline only, if both '''
-        ''' also looks into which means of routing has been made available (which ISP if offline) '''
-        # if Router.route( body.decode('utf-8')):
-        if not "text" in json_body:
-            log_trace('poorly formed message - text missing')
-            ''' acks so that the message does not go back to the queue '''
-
-        if not "number" in json_body:
-            log_trace('poorly formed message - number missing')
-            ''' acks so that the message does not go back to the queue '''
-
-        try:
-            results=None
-            router = Router(url=router_url, priority_offline_isp=offline_isp)
-
-            # online routing '''
-            if configs['ROUTE']['mode'] == '0':
-                results = router.route_online(data={"text":json_body['text'], "number":json_body['number']})
-
-            # offline routing '''
-            elif configs['ROUTE']['mode'] == '1':
-                results = router.route_offline(text=json_body['text'], number=json_body['number'])
-
-            # online, then offline '''
-            elif configs['ROUTE']['mode'] == '2':
-                try:
-                    results = router.route_online(data={"text":json_body['text'], "number":json_body['number']})
-                except Exception as error:
-                    try:
-                        results = router.route_offline(text=json_body['text'], number=json_body['number'])
-                    except Exception as error:
-                        raise Exception(error)
-
-
-            ''' acks that the message has been received (routed) '''
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-            self.logger(f"Routing results: {results}")
-
-        except ConnectionError as error:
-            '''
-            In the event of a network problem (e.g. DNS failure, refused connection, etc), Requests will raise a ConnectionError exception.
-            '''
-            ch.basic_reject( delivery_tag=method.delivery_tag, requeue=True)
-
-        except requests.Timeout as error:
-            '''
-            If a request times out, a Timeout exception is raised.
-            '''
-            ch.basic_reject( delivery_tag=method.delivery_tag, requeue=True)
-
-        except requests.TooManyRedirects as error:
-            '''
-            If a request exceeds the configured number of maximum redirections, a TooManyRedirects exception is raised.
-            '''
-            ch.basic_reject( delivery_tag=method.delivery_tag, requeue=True)
-
-        except Exception as error:
-            ch.basic_reject( delivery_tag=method.delivery_tag, requeue=True)
-            log_trace(traceback.format_exc(), show=True)
-        """
-
-
     def __sms_outgoing_callback(self, ch, method, properties, body):
         # TODO: verify data coming in is actually a json
         json_body = json.loads(body.decode('unicode_escape'))
@@ -542,69 +443,6 @@ class Node:
             self.__update_status(Node.Category.SUCCESS)
             # self.__event_watch(category)
 
-
-
-    def __watchdog_incoming(self):
-        # print('restart watchdog...')
-        try:
-            publish_connection, publish_channel = self.__create_channel(
-                    connection_url=config['NODE']['connection_url'],
-                    queue_name=config['NODE']['routing_queue_name'],
-                    durable=True)
-
-            self.logger('watchdog incoming gone into effect...')
-            messages=Modem(self.m_index).SMS.list('received')
-
-            modem = Modem(self.m_index)
-            while(Deku.modem_ready(self.m_index)):
-                # self.logger('checking for incoming messages...')
-                # messages=modem.SMS.list('received')
-                for msg_index in messages:
-                    sms=Modem.SMS(index=msg_index)
-
-
-                    ''' should this message be deleted or left '''
-                    ''' if deleted, then only the same gateway can send it further '''
-                    ''' if not deleted, then only the modem can send the message '''
-                    ''' given how reabbit works, the modem can't know when messages are sent '''
-                    publish_channel.basic_publish(
-                            exchange='',
-                            routing_key=config['NODE']['routing_queue_name'],
-                            body=json.dumps({"text":sms.text, "number":sms.number}),
-                            properties=pika.BasicProperties( 
-                                delivery_mode=2))
-                    ''' delete messages from here '''
-                    ''' use mockup so testing can continue '''
-                    # modem.delete(msg_index)
-
-                messages=[]
-
-
-                time.sleep(int(config['MODEMS']['sleep_time']))
-
-        except Exception as error:
-            # raise Exception(error)
-            # self.logger(error)
-            log_trace(traceback.format_exc())
-        finally:
-            # modem is no longer available
-            try:
-                self.logger("watchdog incoming: Closing node...", output='stderr')
-
-                # self.routing_consume_channel.stop_consuming()
-                self.routing_consume_connection.close(reply_code=1, reply_text='modem no longer available')
-            except Exception as error:
-                # raise Exception(error)
-                # self.logger(error)
-                log_trace(traceback.format_exc())
-            finally:
-                ''' this finally because when connection is closed
-                an exception is thrown '''
-                if self.m_index in l_threads:
-                    del l_threads[self.m_index]
-
-            ''' do whatever is required to cleanly end this node '''
-
     def __watchdog_monitor(self):
         '''
         - monitors state of modem, kills consumer if modem disconnects
@@ -639,34 +477,6 @@ class Node:
                     del l_threads[self.m_index]
 
             ''' do whatever is required to cleanly end this node '''
-
-    def routing_start_consuming(self):
-        # wd = threading.Thread(target=self.__watchdog, daemon=True)
-        ''' starts watchdog to check if modem is still plugged in '''
-        wd = threading.Thread(target=self.__watchdog_incoming, daemon=True)
-        wd.start()
-
-        self.logger('routing: waiting for message...')
-        try:
-            ''' messages to be routed '''
-            self.logger('routing consumption starting...')
-            self.routing_consume_channel.start_consuming() #blocking
-            # wd.join()
-
-        except pika.exceptions.ConnectionWrongStateError as error:
-            # self.logger(f'Request from Watchdog - \n\t {error}', output='stderr')
-            log_trace(traceback.format_exc())
-        except pika.exceptions.ChannelClosed as error:
-            # self.logger(f'Request from Watchdog - \n\t {error}', output='stderr')
-            log_trace(traceback.format_exc())
-        except Exception as error:
-            # self.logger(f'{self.me} Generic error...\n\t {error}', output='stderr')
-            log_trace(traceback.format_exc())
-        finally:
-            del l_threads[self.m_index]
-
-        # self.logger('ending consumption....')
-
 
     def start_consuming(self):
         # wd = threading.Thread(target=self.__watchdog, daemon=True)
@@ -764,14 +574,9 @@ def master_watchdog(config):
                         continue
 
                     try:
-                        outgoing_node=Node(m_index, m_isp, config, config_event_rules, start_router=False)
+                        outgoing_node=Node(m_index, m_isp, config, config_event_rules)
                         # print(outgoing_node, outgoing_node.__dict__)
                         outgoing_thread=threading.Thread(target=outgoing_node.start_consuming, daemon=True)
-
-                        '''
-                        routing_node=Node(m_index, m_isp)
-                        routing_thread=threading.Thread(target=routing_node.routing_start_consuming, daemon=True)
-                        '''
 
                         # l_threads[m_index] = [outgoing_thread, routing_thread]
                         l_threads[m_index] = [outgoing_thread]
