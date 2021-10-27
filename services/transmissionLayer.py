4270308b732909bc49d6acda0f0a2d97f280a53a
sherlock wisdom <wisdomnji@gmail.com>
2021-10-27 16:39:04 +0100

    update: requirements update and extensions being better handled

--- services/transmissionLayer.py
+++ services/transmissionLayer.py
@@ -2,6 +2,7 @@
 
 ''' Telegram '''
 from transmissionLayer_telegram import TelegramTransmissionLayer
+from CustomConfigParser.customconfigparser import CustomConfigParser
 
 class TransmissionLayer:
     @classmethod
@@ -15,13 +16,16 @@ class TransmissionLayer:
             cls.transmission_layers.append(cls.telegram)
         except CustomConfigParser.NoDefaultFile as error:
             raise(error)
+            # print(error)
         except CustomConfigParser.ConfigFileNotFound as error:
             ''' with this implementation, it stops at the first exception - intended?? '''
             raise(error)
+            # print(error)
         except CustomConfigParser.ConfigFileNotInList as error:
             raise(error)
+            # print(error)
         except Exception as error:
-            raise(error)
+            print(error)
 
 
     # def send(self, data, tranmission_platform='telegram'):
