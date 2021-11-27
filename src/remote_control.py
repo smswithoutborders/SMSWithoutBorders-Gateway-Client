2395b74d5046f2261694412d4ec5ebca8af56e5d
Wisdom Nji <wisdomnji@gmail.com>
2021-11-27 22:54:27 +0100

    update: new exception MissingExecutionValue

--- src/remote_control.py
+++ src/remote_control.py
@@ -6,7 +6,7 @@ import configparser
 
 class RemoteControl:
 
-    class Commands:
+    class Whitelist:
         @staticmethod
         def list():
             path_remote_control = os.path.join(os.path.dirname(__file__), 
@@ -15,9 +15,27 @@ class RemoteControl:
             config = configparser.ConfigParser()
             config.read(path_remote_control)
             
-            return config
+            return config['WHITELIST']
+
+    class Commands:
+        @staticmethod
+        def list(cmd_type):
+            path_remote_control = os.path.join(os.path.dirname(__file__), 
+                    '../.configs', f'remote_control.ini')
+
+            config = configparser.ConfigParser()
+            config.read(path_remote_control)
+            
+            if cmd_type == '$':
+                return config['COMMANDS']
+            return []
 
     class InvalidCommand(Exception):
+        def __init__(self, message):
+            self.message=message
+            super().__init__(self.message)
+
+    class MissingExecutionValue(Exception):
         def __init__(self, message):
             self.message=message
             super().__init__(self.message)
@@ -34,42 +52,57 @@ class RemoteControl:
     def is_executable(text):
         try:
             cmd_type, cmd = RemoteControl.__parser__(text)
-            if cmd_type == '$' and cmd in RemoteControl.Commands.list()['PROGRAMMED']:
+            if cmd in RemoteControl.Commands.list(cmd_type):
                 return True
         except RemoteControl.InvalidCommand as error:
             return False
+        except Exception as error:
+            raise error
         return False
 
     @staticmethod
-    def is_whitelisted(number):
-        return False
+    def is_whitelist(number):
+        return number in RemoteControl.Whitelist.list()
 
     @staticmethod
     def execute(text):
-        raise RemoteControl.InvalidCommand(text)
+        cmd_type, cmd = RemoteControl.__parser__(text)
+        commands = RemoteControl.Commands.list(cmd_type)
+        if cmd in commands:
+            cmd_exec = commands[cmd]
+            if cmd_exec != '':
+                return True
+            else:
+                raise RemoteControl.MissingExecutionValue(text)
+        else:
+            raise RemoteControl.InvalidCommand(text)
 
 class TestRemoteControl(unittest.TestCase):
     def test_parser(self):
-        text = "$ reboot now"
-        self.assertEqual(RemoteControl.__parser__(text), ('$', 'reboot now'))
+        text = "$ test_reboot now"
+        self.assertEqual(RemoteControl.__parser__(text), ('$', 'test_reboot now'))
 
     def test_is_executable(self):
-        text = "$ reboot"
+        text = "$ test_reboot"
         self.assertTrue(RemoteControl.is_executable(text))
 
-    def test_is_whitelisted(self):
-        number = "+237000000000"
-        self.assertTrue(RemoteControl.is_whitelisted(number))
+    def test_is_whitelist(self):
+        number = "+000000000"
+        self.assertTrue(RemoteControl.is_whitelist(number))
 
     def test_execute(self):
-        text = "$ reboot"
+        text = "$ test_reboot"
         self.assertTrue(RemoteControl.execute(text))
 
-    def test_execute_raises_exception(self):
-        invalid_text = "$reboot"
-        with self.assertRaises(RemoteControl.InvalidCommand, 
-                RemoteControl.execute(invalid_text)) as excp:
-            self.assertEqual(excp.message, invalid_text)
+    def test_execute_raises_exception_invalid_command(self):
+        invalid_text = "$test_reboot"
+        with self.assertRaises(RemoteControl.InvalidCommand) as excp:
+            RemoteControl.execute(invalid_text)
+
+    def test_execute_raises_missing_execution(self):
+        text = "$ test_reboot"
+        with self.assertRaises(RemoteControl.MissingExecutionValue) as excp:
+            RemoteControl.execute(text)
 
 if __name__ == '__main__':
     unittest.main()
