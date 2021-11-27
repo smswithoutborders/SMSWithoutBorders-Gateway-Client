aa38c996d657ffaca1ae54c5ce8cf287ad0109db
Wisdom Nji <wisdomnji@gmail.com>
2021-11-27 22:20:00 +0100

    update: executable works, __parser__ works

--- src/remote_control.py
+++ src/remote_control.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+import os
 import unittest
+import configparser
 
 class RemoteControl:
+
+    class Commands:
+        @staticmethod
+        def list():
+            path_remote_control = os.path.join(os.path.dirname(__file__), 
+                    '../.configs', f'remote_control.ini')
+
+            config = configparser.ConfigParser()
+            config.read(path_remote_control)
+            
+            return config
 
     class InvalidCommand(Exception):
         def __init__(self, message):
@@ -10,7 +23,21 @@ class RemoteControl:
             super().__init__(self.message)
 
     @staticmethod
+    def __parser__(text):
+        s_text = text.split(' ')
+        if len(s_text) < 2:
+            raise RemoteControl.InvalidCommand(text)
+
+        return s_text[0], ' '.join(s_text[1:])
+
+    @staticmethod
     def is_executable(text):
+        try:
+            cmd_type, cmd = RemoteControl.__parser__(text)
+            if cmd_type == '$' and cmd in RemoteControl.Commands.list()['PROGRAMMED']:
+                return True
+        except RemoteControl.InvalidCommand as error:
+            return False
         return False
 
     @staticmethod
@@ -22,6 +49,10 @@ class RemoteControl:
         raise RemoteControl.InvalidCommand(text)
 
 class TestRemoteControl(unittest.TestCase):
+    def test_parser(self):
+        text = "$ reboot now"
+        self.assertEqual(RemoteControl.__parser__(text), ('$', 'reboot now'))
+
     def test_is_executable(self):
         text = "$ reboot"
         self.assertTrue(RemoteControl.is_executable(text))
@@ -36,9 +67,9 @@ class TestRemoteControl(unittest.TestCase):
 
     def test_execute_raises_exception(self):
         invalid_text = "$reboot"
-        with self.assertRaises(RemoteControl.InvalidCommand, RemoteControl.execute(invalid_text)) as excp:
+        with self.assertRaises(RemoteControl.InvalidCommand, 
+                RemoteControl.execute(invalid_text)) as excp:
             self.assertEqual(excp.message, invalid_text)
-
 
 if __name__ == '__main__':
     unittest.main()
