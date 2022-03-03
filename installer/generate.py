fcf46abed3a2b49805fe13ed3963e6d1a516ed15
sherlock wisdom <wisdomnji@gmail.com>
2022-03-03 15:52:01 +0100

    update: changed from gateway to outgoing

--- installer/generate.py
+++ installer/generate.py
@@ -69,7 +69,7 @@ def generate_systemd():
         # distro_systemd_schemas_gateway[dist]['Unit']['BindsTo'] = "deku_rabbitmq.service"
         distro_systemd_schemas_gateway[dist]['Unit']['Wants'] = "ModemManager.service"
         distro_systemd_schemas_gateway[dist]['Service']['ExecStart'] = \
-                f"+{path_venv}/bin/python3 {path_main} --log=INFO --module=gateway"
+                f"+{path_venv}/bin/python3 {path_main} --log=INFO --module=outgoing"
 
     for dist in distro_systemd_schemas_cluster:
         distro_systemd_schemas_cluster[dist]['Unit']['Description'] = "Deku Cluster service"
