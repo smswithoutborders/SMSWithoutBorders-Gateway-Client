f0f310cb9c560d40cf419364d09902c24b7fe9b2
sherlock <wisdomnji@gmail.com>
2022-03-17 12:32:18 +0100

    update: added pre-commit hook in configs and makefile

--- deps/rabbitmq/init.sh
+++ deps/rabbitmq/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-tar -xf /home/sherlock/Desktop/SMSWithoutBorders-Gateway-Client/installer/../deps/rabbitmq/rabbitmq-server-generic-unix-3.9.9.tar.xz -C /home/sherlock/Desktop/SMSWithoutBorders-Gateway-Client/installer/../deps/rabbitmq/builds/
+tar -xf /home/sherlock/Desktop/afkanerd/SMSWithoutBorders-Gateway-Client/installer/../deps/rabbitmq/rabbitmq-server-generic-unix-3.9.9.tar.xz -C /home/sherlock/Desktop/afkanerd/SMSWithoutBorders-Gateway-Client/installer/../deps/rabbitmq/builds/
 
-cp /home/sherlock/Desktop/SMSWithoutBorders-Gateway-Client/installer/files/rabbitmq-env.conf /home/sherlock/Desktop/SMSWithoutBorders-Gateway-Client/deps/rabbitmq/builds/rabbitmq_server-3.9.9/etc/rabbitmq/rabbitmq-env.conf
+cp /home/sherlock/Desktop/afkanerd/SMSWithoutBorders-Gateway-Client/installer/files/rabbitmq-env.conf /home/sherlock/Desktop/afkanerd/SMSWithoutBorders-Gateway-Client/deps/rabbitmq/builds/rabbitmq_server-3.9.9/etc/rabbitmq/rabbitmq-env.conf
