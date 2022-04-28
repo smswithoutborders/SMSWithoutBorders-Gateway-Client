159bf6bddd63d804aedb93d5f2bf795fa83ecdc7
sherlock wisdom <wisdomnji@gmail.com>
2022-04-28 12:05:21 +0100

    update: producer test included

--- src/test/producer.py
+++ src/test/producer.py
@@ -6,51 +6,37 @@ import json
 import configparser
 
 if __name__ == "__main__":
+    """
     config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
     config.read(os.path.join(os.path.dirname(__file__), '../configs', 'config.ini'))
 
     username=config['NODE']['api_id']
     password=config['NODE']['api_key']
+    """
     credentials=pika.credentials.PlainCredentials(
-            username=username,
-            password=password)
+            username='guest',
+            password='guest')
 
-    # TODO: port should come from config
-    connection_url=config['NODE']['connection_url']
+    connection_url='localhost'
     parameters=pika.ConnectionParameters(connection_url, 5672, '/', credentials)
     connection=pika.BlockingConnection(parameters=parameters)
 
-    '''
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=config['NODE']['connection_url']))
-    '''
-
     channel = connection.channel()
 
-    # queue_name = config_queue_name + _ + isp
-    '''
-    queue_name = config['NODE']['outgoing_queue_name'] + '_' + sys.argv[1].lower()
-    routing_key = config['NODE']['outgoing_queue_name'] + '.' + sys.argv[1]
-    '''
-    queue_name=config['NODE']['api_id'] + '_' + config['NODE']['outgoing_queue_name'] + '_' + sys.argv[1].lower()
-    routing_key=config['NODE']['api_id'] + '_' + config['NODE']['outgoing_queue_name'] + '.' + sys.argv[1].lower()
-    
-    '''
-    channel.exchange_declare( 
-            exchange=config['NODE']['outgoing_exchange_name'], exchange_type=config['NODE']['outgoing_exchange_type'])
-    '''
-    number = sys.argv[2]
-    message = ' '.join(sys.argv[3:]) or "Hello World!"
-    data = json.dumps({"text":message, "number":number})
+    routing_key='inbound.route.route'
+    number = sys.argv[1]
 
-    channel.basic_publish(
-        exchange=config['NODE']['outgoing_exchange_name'],
-        # routing_key=queue_name.replace('_', '.'),
-        routing_key=routing_key,
-        body=data,
-        properties=pika.BasicProperties(
-            delivery_mode=2,  # make message persistent
-        ))
+    message = ' '.join(sys.argv[2:]) or "Hello World!"
+    data = json.dumps({"text":message, "MSISDN":number})
+
+    channel.basic_publish( 
+            exchange='',
+            routing_key=routing_key,
+            body=data,
+            properties=pika.BasicProperties(
+                delivery_mode=2,  # make message persistent
+            )
+    )
     # print(" [x] Sent %r" % message)
     print(f"[x] message; {message} ::\n\tsent to {number}")
     connection.close()
